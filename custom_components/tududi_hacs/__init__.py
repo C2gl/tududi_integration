46c3015402f6c324d479a08bc47a0c19bb7e3781
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-17 21:18:16 +0200

    Revert "User popup"

--- custom_components/tududi_hacs/__init__.py
+++ custom_components/tududi_hacs/__init__.py
@@ -1,24 +1,16 @@
 from homeassistant.core import HomeAssistant
-from homeassistant.components.http.static import StaticPathConfig
-
 
 DOMAIN = "tududi_hacs"
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    await hass.http.async_register_static_paths([
-        StaticPathConfig(
-            url_path="/tududi-panel",
-            file_path=hass.config.path("www/tududi_hacs/panel.html"),
-            cache_headers=False,
-        )
-    ])
-    hass.components.frontend.async_register_panel(
-        component_name="custom",
-        frontend_url_path="tududi",
+    hass.components.frontend.async_register_built_in_panel(
+        component_name="iframe",
         sidebar_title="Tududi",
         sidebar_icon="mdi:checkbox-marked-outline",
-        config={},
-        require_admin=False,
-        module_url="/tududi-panel",
+        frontend_url_path="tududi",
+        config={
+            "url": "[your Tududi URL]",
+        },
+        require_admin=False
     )
     return True
