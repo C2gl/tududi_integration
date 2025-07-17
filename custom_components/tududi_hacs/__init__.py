6a2d5b12d65bc896abc48ae3fd676305710662f3
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-17 20:20:39 +0200

    io error due to wrong call

--- custom_components/tududi_hacs/__init__.py
+++ custom_components/tududi_hacs/__init__.py
@@ -3,7 +3,7 @@ from homeassistant.core import HomeAssistant
 DOMAIN = "tududi_hacs"
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    hass.http.register_static_path(
+    await hass.http.register_static_path(
         "/tududi-panel",
         hass.config.path("www/tududi_hacs/panel.html"),
         cache_headers=False,
