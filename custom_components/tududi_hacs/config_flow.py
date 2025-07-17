e067a57d83a576715a462a1e62f7d5cf0afc3024
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-17 19:20:56 +0200

    tentative url script in panel.html

--- custom_components/tududi_hacs/config_flow.py
+++ custom_components/tududi_hacs/config_flow.py
@@ -2,3 +2,20 @@ from homeassistant import config_entries
 import voluptuous as vol
 from homeassistant.core import callback
 
+DOMAIN = "tududi_hacs" # domain mentioned in manifest.json
+
+class MyIntegrationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for your HACS integration."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="TuDudi Panel", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("url"): str
+            })
+        )
