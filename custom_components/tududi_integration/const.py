73329bb850f33ba1e86ad68e341ddb6d8bbb4a6e
C2gl <lievens.guillaume@gmail.com>
2026-01-07 14:47:45 +0100

    update const for api info

--- custom_components/tududi_integration/const.py
+++ custom_components/tududi_integration/const.py
@@ -6,8 +6,7 @@ DOMAIN = "tududi_integration"
 CONF_URL = "url"
 CONF_TITLE = "title"
 CONF_ICON = "icon"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
+CONF_API_KEY = "api_key"  
 
 # Defaults
 DEFAULT_TITLE = "Tududi"
@@ -16,3 +15,7 @@ DEFAULT_ICON = "mdi:clipboard-text"
 # Sensor constants
 SENSOR_UPDATE_INTERVAL = 300  # 5 minutes
 SENSOR_TIMEOUT = 30  # 30 seconds
+
+# API Endpoints
+API_VERSION = "v1"
+API_BASE_PATH = "/api/v1"
